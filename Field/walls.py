deb728fa88a2124a72c38d8329917dd6833dec51
sebastian_kup <sebastian.kuppa@web.de>
2022-08-16 17:26:50 +0200

    added class Wall with init and create_hole()

---
+++ Field/walls.py
@@ -0,0 +1,22 @@
+import numpy as np
+from random import randint
+
+
+class Wall:
+    def __init__(self, height=5, width=100, hole_width=20,
+                 y=0, speed=1, field=None):
+        self.height = height
+        self.width = width
+        self.hole_width = hole_width
+        self.y = y
+        self.speed = speed
+        self.field = field
+        self.body_unit = 1
+        self.body = np.ones((self.height, self.width))*self.body_unit
+        self.out_of_range = False
+        self.create_hole()
+
+    def create_hole(self):
+        hole = np.zeros((self.height, self.hole_width))
+        hole_pos = randint(0, self.width - self.hole_width)
+        self.body[:, hole_pos:hole_pos+self.hole_width] = 0
